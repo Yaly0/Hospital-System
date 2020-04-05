e994948792e5075e15069646470196cc95b9da0f
yaly1 <yaly1@etf.unsa.ba>
2020-04-05 11:22:56 +0200

    Class "Person" added

---
+++ src/ba/unsa/etf/rpr/projekat/Person.java
@@ -0,0 +1,67 @@
+package ba.unsa.etf.rpr.projekat;
+
+
+import java.time.LocalDate;
+
+public abstract class Person {
+    protected String firstName, lastName, citizenNumber, phoneNumber;
+    protected boolean isMale; // true: male, false: female
+    protected int id;
+    protected LocalDate birthDate;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCitizenNumber() {
+        return citizenNumber;
+    }
+
+    public void setCitizenNumber(String citizenNumber) {
+        this.citizenNumber = citizenNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public boolean isMale() {
+        return isMale;
+    }
+
+    public void setMale(boolean male) {
+        isMale = male;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
