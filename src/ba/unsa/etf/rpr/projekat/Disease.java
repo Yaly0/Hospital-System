3e76effc863ad7498430ebf1a7792132b78adb7d
yaly1 <yaly1@etf.unsa.ba>
2020-09-08 15:44:57 +0200

    Class "Disease" added

---
+++ src/ba/unsa/etf/rpr/projekat/Disease.java
@@ -0,0 +1,42 @@
+package ba.unsa.etf.rpr.projekat;
+
+public class Disease {
+    private int id;
+    private String diseaseName;
+    private MedicalMajor medicalMajor;
+
+    public Disease(int id, String diseaseName, MedicalMajor medicalMajor) {
+        this.id = id;
+        this.diseaseName = diseaseName;
+        this.medicalMajor = medicalMajor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDiseaseName() {
+        return diseaseName;
+    }
+
+    public void setDiseaseName(String diseaseName) {
+        this.diseaseName = diseaseName;
+    }
+
+    public MedicalMajor getMedicalMajor() {
+        return medicalMajor;
+    }
+
+    public void setMedicalMajor(MedicalMajor medicalMajor) {
+        this.medicalMajor = medicalMajor;
+    }
+
+    @Override
+    public String toString() {
+        return diseaseName;
+    }
+}
