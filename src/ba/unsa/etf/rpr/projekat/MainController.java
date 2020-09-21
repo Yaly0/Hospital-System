d2e1eccf2f02fae528439f3a23a77551a9376c7b
yaly1 <yaly1@etf.unsa.ba>
2020-09-21 09:11:01 +0200

    Attributes added in "MainController"

--- src/ba/unsa/etf/rpr/projekat/MainController.java
+++ src/ba/unsa/etf/rpr/projekat/MainController.java
@@ -1,5 +1,48 @@
 package ba.unsa.etf.rpr.projekat;
 
+import javafx.collections.ObservableList;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class MainController {
 
+    public TableView<Appointment> tableViewAppointments;
+    public TableView<Patient> tableViewPatients;
+    public TableView<Doctor> tableViewDoctors;
+    public TableView<Treatment> tableViewTreatments;
+    public TableView<MedicalMajor> tableViewMedicalMajors;
+    public TableView<Disease> tableViewDiseases;
+    public TableColumn columnAppointmentsId;
+    public TableColumn columnAppointmentsPatient;
+    public TableColumn columnAppointmentsDoctor;
+    public TableColumn columnAppointmentsDiseases;
+    public TableColumn columnAppointmentsDate;
+    public TableColumn columnAppointmentsTime;
+    public TableColumn columnPatientsId;
+    public TableColumn columnPatientsFirstName;
+    public TableColumn columnPatientsLastName;
+    public TableColumn columnPatientsBirthDate;
+    public TableColumn columnPatientsCitizenNumber;
+    public TableColumn columnPatientsPhoneNumber;
+    public TableColumn columnDoctorsId;
+    public TableColumn columnDoctorsFirstName;
+    public TableColumn columnDoctorsLastName;
+    public TableColumn columnDoctorsBirthDate;
+    public TableColumn columnDoctorsMedicalMajor;
+    public TableColumn columnDoctorsPhoneNumber;
+    public TableColumn columnTreatmentsId;
+    public TableColumn columnTreatmentsName;
+    public TableColumn columnMedicalMajorsId;
+    public TableColumn columnMedicalMajorsName;
+    public TableColumn columnDiseasesId;
+    public TableColumn columnDiseasesName;
+    public TableColumn columnDiseasesMedicalMajor;
+
+    private HospitalDAO dao;
+    private ObservableList<Appointment> listAppointments;
+    private ObservableList<Patient> listPatients;
+    private ObservableList<Doctor> listDoctors;
+    private ObservableList<Treatment> listTreatments;
+    private ObservableList<MedicalMajor> listMedicalMajors;
+    private ObservableList<Disease> listDiseases;
 }
